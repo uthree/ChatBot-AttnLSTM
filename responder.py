a61e10b658ae56fa7b7d7bd4a44147f75629041a
uthree <33905956+uthree@users.noreply.github.com>
2021-10-02 08:17:39 +0900

    update

--- responder.py
+++ responder.py
@@ -18,7 +18,7 @@ class Responder:
     def train(self, dataset, num_epoch=1, batch_size=1, device=torch.device('cpu')):
         self.s2s.train()
         self.s2s.to(device)
-        optimizer = optim.Adam(self.s2s.parameters(), lr=0.0001)
+        optimizer = optim.Adam(self.s2s.parameters(), lr=5e-5)
         criterion = nn.CrossEntropyLoss(ignore_index=self.paddng_idx)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
         bar = tqdm(total=len(dataset) * num_epoch)
@@ -31,19 +31,21 @@ class Responder:
                 loss = criterion(torch.flatten(output, start_dim=0, end_dim=1), torch.flatten(tgt[:, 1:], start_dim=0, end_dim=1))
                 loss.backward()
                 optimizer.step()
-                bar.set_description(f'Epoch: {epoch}/{num_epoch}, Loss: {loss.item():.4f}')
+                bar.set_description(f'Epoch: {epoch}/{num_epoch}, Loss: {loss.item():.4f}, Iter: {i}')
                 bar.update(batch_size)
                 if i % 3000 == 0:
                     torch.save(self.s2s, './seq2seq.pt')
+                    tqdm.write(f'Saved model at epoch {epoch}, iter={i}')
             torch.save(self.s2s, './seq2seq.pt')
         bar.close()
 
-    def predict_tensor(self, src, device=torch.device('cpu'), max_output_len = 64):
+    def predict_tensor(self, src, device=torch.device('cpu'), max_output_len = 64, noise_gain=0.0):
         self.s2s.eval()
         self.s2s.to(device)
         src = src.to(device)
-        tgt = torch.zeros(src.shape[0], 1).long().to(device)
+        tgt = torch.ones(src.shape[0], 1).long().to(device)
         memory, state = self.s2s.encoder(self.s2s.embedding(src))
+        state = [(h + noise_gain * torch.randn_like(h), c + noise_gain * torch.randn_like(c)) for h, c in state]
         for i in range(max_output_len):
             output, state = self.s2s.decoder(self.s2s.embedding(tgt[:, -1:]), memory, state)
             output_argmax = self.s2s.hid2vocab(output).argmax(dim=-1)
@@ -63,10 +65,10 @@ class Responder:
                 res.append(id)
         return res
 
-    def predict_sentences(self, sentences, device=torch.device('cpu'), max_output_len = 64):
+    def predict_sentences(self, sentences, device=torch.device('cpu'), max_output_len = 64, noise_gain=0.0):
         src = [self.spm.EncodeAsIds(sentence) for sentence in sentences]
         src = torch.tensor(src).long()
-        tgt = self.predict_tensor(src, device, max_output_len)
+        tgt = self.predict_tensor(src, device, max_output_len, noise_gain=noise_gain)
         tgt = [self.padding_after_eos(ids) for ids in tgt.tolist()]
         return [self.spm.DecodeIdsWithCheck(tgt[i]) for i in range(len(sentences))]
 
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from dialogue_dataset import DialogueDataset
 from responder import Responder
+import time
 
 import torch
 import torch.nn as nn
@@ -7,12 +8,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 responder = Responder.load('seq2seq.pt', './sentencepiece.model')
-join_past_logs = 3
+join_past_logs = 1
 logs = []
 while True:
     user_utterance = input('USER > ')
     logs.append(user_utterance)
-    bot_responce = responder.predict_sentences(['<sep>'.join(logs[-join_past_logs:])])[0]
+    bot_responce = responder.predict_sentences(['<sep>'.join(logs[-join_past_logs:])], noise_gain=0.3)[0]
     bot_responce = bot_responce.replace('<sep>', '\n')
-    print('BOT > ' + bot_responce)
+    for resp in bot_responce.split('\n'):
+        print('BOT > ' + resp)
+        #time.sleep(1)
     logs.append(bot_responce)
